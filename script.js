daedfc4cecd508a0f7d1a301b2eb570454585729
Nayyer <nayyer.amd@gmail.com>
2022-08-29 00:30:18 +0300

    first

---
+++ script.js
@@ -0,0 +1,13 @@
+
+// to add scroll effect in the navigation bar
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navigation').addClass("sticky");
+        }else{
+            $('.navigation').removeClass("sticky");
+        }
+    });
+   // script for toggling naviation
+   $
+});
